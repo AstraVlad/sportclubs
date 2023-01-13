ea98eff539db36e1618b14f03e6ecf759df77536
Yevgeniy <yev.bor@yandex.ru>
2023-01-13 16:58:03 +0200

    create two new files and update vite config

--- SportClubsFrontend/vite.config.ts
+++ SportClubsFrontend/vite.config.ts
@@ -1,7 +1,23 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
+const root = resolve(__dirname, 'src')
+const outDir = resolve(__dirname, 'dist')
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root,
+  build: {
+    outDir,
+    emptyOutDir: true, //to override dist folder
+    rollupOptions: {
+      input: {
+        main: resolve(root, 'index.html'),
+        guest: resolve(root, 'guest.html'),
+      }
+    }
+  }
 })
+
