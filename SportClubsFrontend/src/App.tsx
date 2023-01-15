2b6b7dcd3b3bd9873eb105f5bd0de9323e5c15fc
Yevgeniy <yev.bor@yandex.ru>
2023-01-15 23:46:48 +0200

    created redux toolkit

--- SportClubsFrontend/src/App.tsx
+++ SportClubsFrontend/src/App.tsx
@@ -2,6 +2,7 @@ import './App.css'
 import {Routes, Route} from 'react-router-dom';
 import SignIn from './components/SignIn'
 import ResetForm from './components/ResetForm';
+import { Counter } from './features/counter/Counter';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>    
         <Route path='/signin' element={<SignIn />}/>
         <Route path='/forgot' element={<ResetForm />}/>
+        <Route path='/redux-test' element={<Counter/>}/>
       </Routes>
     </div>
   )
--- SportClubsFrontend/src/main.tsx
+++ SportClubsFrontend/src/main.tsx
@@ -4,12 +4,16 @@ import App from './App'
 import theme from './styles/styles'
 import { BrowserRouter} from "react-router-dom";
 import { ThemeProvider } from '@mui/material/styles';
+import { store } from './store'
+import { Provider } from 'react-redux'
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+      <Provider store={store}>
         <BrowserRouter>
-        <ThemeProvider theme={theme}>
-          <App />
-        </ThemeProvider>
+          <ThemeProvider theme={theme}>
+            <App />
+          </ThemeProvider>
         </BrowserRouter>
+      </Provider>
   </React.StrictMode>,
 )
